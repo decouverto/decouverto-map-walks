7709298fbec39cd099fd3e4f186722b66fdcf1bd
Cédric JUNG <cedced19@gmail.com>
2020-07-14 13:47:02 +0200

    Use Jquery

--- dev/scripts/index.js
+++ dev/scripts/index.js
@@ -1,15 +1,22 @@
 
 var getJSON = require('./get-json.js');
 
+
+var element = document.getElementById('popup');
+
+
+
+
 getJSON('/walks.json', function (err, data) {
     if (err) return console.error(err);
-    
+
     var markerSource = new ol.source.Vector();
 
     function addMarker(lon, lat, title) {
 
         var iconFeature = new ol.Feature({
-            geometry: new ol.geom.Point(ol.proj.transform([lon, lat], 'EPSG:4326', 'EPSG:3857'))
+            geometry: new ol.geom.Point(ol.proj.transform([lon, lat], 'EPSG:4326', 'EPSG:3857')),
+            title: title
         });
 
         iconFeature.setStyle(new ol.style.Style({
@@ -19,16 +26,7 @@ getJSON('/walks.json', function (err, data) {
                 anchorYUnits: 'pixels',
                 opacity: 0.75,
                 src: '/images/marker_icon.png'
-            })),
-            text: new ol.style.Text({
-                offsetY: -40,
-                font: '14px Calibri,sans-serif',
-                fill: new ol.style.Fill({ color: '#e74c3c' }),
-                stroke: new ol.style.Stroke({
-                    color: '#fff', width: 2
-                }),
-                text: title
-            })
+            }))
         }));
 
         markerSource.addFeature(iconFeature);
@@ -49,8 +47,48 @@ getJSON('/walks.json', function (err, data) {
             zoom: 0
         })
     });
+
+    var popup = new ol.Overlay({
+        element: element,
+        positioning: 'bottom-center',
+        stopEvent: false,
+        offset: [0, -50]
+    });
+    map.addOverlay(popup);
+    map.on('click', function (e) {
+        var feature = map.forEachFeatureAtPixel(e.pixel,
+            function (feature) {
+                return feature;
+            });
+        if (feature) {
+            var coordinates = feature.getGeometry().getCoordinates();
+            popup.setPosition(coordinates);
+            $(element).popover({
+                placement: 'top',
+                html: true,
+                content: feature.get('title')
+            });
+            $(element).popover('show');
+        } else {
+            $(element).popover('destroy');
+        }
+    });
+    map.on('pointermove', function(e) {
+        if (e.dragging) {
+          $(element).popover('destroy');
+          return;
+        }
+        var hit = this.forEachFeatureAtPixel(e.pixel, function(feature, layer) {
+            return true;
+        }); 
+        if (hit) {
+            this.getTargetElement().style.cursor = 'pointer';
+        } else {
+            this.getTargetElement().style.cursor = '';
+        }
+      });
+
     // set center
-    
     map.getView().setZoom(8);
 
     // set markers
@@ -67,7 +105,7 @@ getJSON('/walks.json', function (err, data) {
         barycentre.latitude += el.coord.latitude
         barycentre.n += 1
     });
-    barycentre.longitude/=barycentre.n
-    barycentre.latitude/=barycentre.n
+    barycentre.longitude /= barycentre.n
+    barycentre.latitude /= barycentre.n
     map.getView().setCenter(ol.proj.transform([barycentre.longitude, barycentre.latitude], 'EPSG:4326', 'EPSG:3857'));
 });
